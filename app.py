796530682896fda8d857109a9b50f36bdc836198
AnaMCarrenoA <128761853+AnaMCarrenoA@users.noreply.github.com>
2023-08-16 22:41:10 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(name)
+
+@app.route('/init', methods = ['GET'])
+def init():
+  return {"payload":"welcome to my project"}
+
+@app.route('/read/:<content>', methods = ['GET'])
+def read(content):
+    return {'payload': content}
+
+
+@app.route('/init/content', methods = ['GET'])
+def admin():
+  return {"payload":alfa}
+
+@app.route('/delete/:<content>', methods = ['DELETE'])
+def read(content):
+    return {"payload":qux}
+
+if name == 'main':
+    app.run(debug=True)
